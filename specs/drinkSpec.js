f5a3c8d3d60f9c8542acd684a731ce3058f203a5
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-17 09:53:49 +0200

    test - testing each drink

--- specs/drinkSpec.js
+++ specs/drinkSpec.js
@@ -5,9 +5,38 @@ describe("My whatCanIDrink function", function(){
   });
 
   describe("Checks age", function(){
+
     it("should exist", function(){
       expect(whatCanIDrink).toBeDefined();
     });
+
+    it("should return drink toddy when called as whatCanIDrink(13)", function() {
+      var result = whatCanIDrink(13)
+      expect(result).toBe("Drink Toddy");
+    });
+
+    it("should return drink toddy when called as whatCanIDrink(17)", function() {
+      var result = whatCanIDrink(17)
+      expect(result).toBe("Drink Coke");
+    });
+
+    it("should return drink toddy when called as whatCanIDrink(18)", function() {
+      var result = whatCanIDrink(18)
+      expect(result).toBe("Drink Beer");
+    });
+
+    it("should return drink toddy when called as whatCanIDrink(20)", function() {
+      var result = whatCanIDrink(20)
+      expect(result).toBe("Drink Beer");
+    });
+
+    it("should return drink toddy when called as whatCanIDrink(30)", function() {
+      var result = whatCanIDrink(30)
+      expect(result).toBe("Drink Whisky");
+    });
+
+
+
   });
 
 });
